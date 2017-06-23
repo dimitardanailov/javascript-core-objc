151ac37cc0091f8e73b96e326c520f12e4f8f2df
Dimitar Danailov <dimityr.danailov@gmail.com>
2017-06-23 18:30:55 +0300

    Initial Commit

---
+++ JavascriptApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JavascriptApp
+//
+//  Created by Dimitar Danailov on 6/23/17.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ JavascriptApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  JavascriptApp
+//
+//  Created by Dimitar Danailov on 6/23/17.
+//
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
